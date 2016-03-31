b970c24d2053375cd3c11164d5703f52112f0401
Debajit Majumder <debajit.business@gmail.com>
2016-03-31 15:34:46 +0530

    Revert "Used hashmap to find the winner"
    This reverts commit a6849482ef23caa708bc096425d6f931877a601f.

--- src/RockPaperScissor.java
+++ src/RockPaperScissor.java
@@ -58,6 +58,8 @@ public class RockPaperScissor{
 	
 	public String findWinner(){
 		String result;
+		String[] winningSet = { "p1", "p2", "r0", "r2", "s0", "s1" }; 
+		String[] winnerMap = { "user", "comp", "comp", "user", "user", "comp" };
 		char userInput = this.getUserInput();
 		int compInput = this.getCompInput();
 		
@@ -66,6 +68,9 @@ public class RockPaperScissor{
 		
 		if( index == -1 ) {
 			result = "tie";
+		}
+		else {
+			result = winnerMap[ index ];
 		}
 		return result;
 	}
